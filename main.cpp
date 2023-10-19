f160510752cc17f57b33f313196e56b5655d5e89
satoisamu2720 <115054959+satoisamu2720@users.noreply.github.com>
2023-10-19 17:40:55 +0900

    ok

--- main.cpp
+++ main.cpp
@@ -1,20 +1,40 @@
 #include <stdio.h>
 
+
+int Recursive(int h, int j, int m) {
+
+	if (h <= 0) {
+		//処理が成功した場合
+		return j;
+	}
+
+	return Recursive(h - 1, j += m, m * 2 - 50);
+
+}
+
+
+
 int TypicallyCalc(int time) {
 	return 1072 * time;
+
 }
 
-int RecursiveCalc(int time, int HourlyWage = 100) {
-	if (time == 1) {
+int RecursiveCalc(int time,int j, int HourlyWage) {
+
+	/*if (time == 1) {
 		return HourlyWage;
+	}*/
+	if (time <= 0) {
+		//処理が成功した場合
+		return j;
 	}
-	return (RecursiveCalc(time - 1, HourlyWage * 2 - 50));
+	return (RecursiveCalc(time - 1,j+= HourlyWage, HourlyWage * 2 - 50));
 }
 
-void ComparisonWage(int time) {
+void ComparisonWage(int time, int j, int HourlyWage) {
 	int Typically = TypicallyCalc(time);
-	int Recursive = RecursiveCalc(time);
-	printf("一般的な賃金 : %d\n", Typically);
+	int Recursive = RecursiveCalc(time,j, HourlyWage);
+	printf("\n一般的な賃金 : %d\n", Typically);
 	printf("再帰的な賃金 : %d\n", Recursive);
 	if (Typically > Recursive) {
 		printf("一般的な賃金体系");
@@ -28,9 +48,12 @@ void ComparisonWage(int time) {
 }
 
 int main() {
-	ComparisonWage(9);
+	
+
+	ComparisonWage(4,0,100);
 
 	return 0;
+
 }
 
 
