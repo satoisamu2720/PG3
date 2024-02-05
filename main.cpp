3fe92edeb1b5a480786d54245b94c3b49a72565a
satoisamu2720 <115054959+satoisamu2720@users.noreply.github.com>
2024-02-05 17:06:41 +0900

    実装完了

--- main.cpp
+++ main.cpp
@@ -1,23 +1,22 @@
 #include <stdio.h>
-#include <random>
-#include <chrono>
 #include <thread>
-#include <iostream>
-#include <functional>
-//#include "MyClass.h"
-#include "Enemy.h"
 
-//std::random_device seed_Gen;
-//std::mt19937 mtrand(seed_Gen());
+void Print1() { printf("thread1\n"); }
+
+void Print2() { printf("thread2\n"); }
+
+void Print3() { printf("thread3\n"); }
 
 int main() {
-	Enemy* enemy_ = new Enemy();
 
-while (enemy_->GetCount() == false) {
-		enemy_->Update();
-	}
+	std::thread th1(Print1);
+	th1.join();
 
+	std::thread th2(Print2);
+	th2.join();
 
+	std::thread th3(Print3);
+	th3.join();
 
 	return 0;
 }
