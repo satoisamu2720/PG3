a78c15995be303d85511e318735ee64da924a2e6
satoisamu2720 <115054959+satoisamu2720@users.noreply.github.com>
2024-02-05 19:14:13 +0900

    実装完了

--- main.cpp
+++ main.cpp
@@ -1,8 +1,73 @@
 #include <Novice.h>
-#include "Player.h"
-#include "InputHandler.h"
+#include <math.h>
+#include <stdio.h>
+
+
 
 const char kWindowTitle[] = "";
+struct Vector2 {
+	float x;
+	float y;
+};
+struct Vector3 {
+	float x;
+	float y;
+	float z;
+};
+struct Matrix4x4 final {
+	float m[4][4];
+};
+
+Matrix4x4 MakeIdentity4x4() { return {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1}; }
+
+float Dot(const Vector3& v1, const Vector3& v2) { return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z; }
+
+float Length(const Vector3& v) { return sqrtf(Dot(v, v)); }
+
+Vector3 Normalise(const Vector3& v) {
+	float len = Length(v);
+	if (len != 0) {
+		return {v.x / len, v.y / len, v.z / len};
+	}
+	return v;
+}
+Matrix4x4 MakeRotateAxisAngle(Vector3 axis, float angle) {
+	axis = Normalise(axis);
+	Matrix4x4 result;
+
+	float cos = cosf(angle);
+	float sin = sinf(angle);
+
+	result = MakeIdentity4x4();
+	result.m[0][0] = (axis.x * axis.x) * (1 - cos) + cos;
+	result.m[0][1] = axis.x * axis.y * (1 - cos) + axis.z * sin;
+	result.m[0][2] = axis.x * axis.z * (1 - cos) - axis.y * sin;
+	result.m[1][0] = axis.x * axis.y * (1 - cos) - axis.z * sin;
+	result.m[1][1] = (axis.y * axis.y) * (1 - cos) + cos;
+	result.m[1][2] = axis.y * axis.z * (1 - cos) + axis.x * sin;
+	result.m[2][0] = axis.x * axis.z * (1 - cos) + axis.x * sin;
+	result.m[2][1] = axis.y * axis.z * (1 - cos) - axis.x * sin;
+	result.m[2][2] = (axis.z * axis.z) * (1 - cos) + cos;
+
+	return result;
+}
+static const int kRowHeight = 20;
+static const int kColummWidth = 60;
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+	for (int row = 0; row < 4; ++row) {
+		for (int columm = 0; columm < 4; ++columm) {
+			Novice::ScreenPrintf(
+			    x + columm * kColummWidth, y + row * kRowHeight, "%6.3f", matrix.m[row][columm],
+			    label);
+		}
+	}
+}
+Vector3 axis = Normalise({1.0f, 1.0f, 1.0f});
+
+float angle = 0.44f;
+
+Matrix4x4 rotateMatrix = MakeRotateAxisAngle(axis, angle);
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -14,13 +79,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char keys[256] = {0};
 	char preKeys[256] = {0};
 
-	InputHandler* inputHandler_ = new InputHandler();
-	inputHandler_->AssignMoveLeftCommand2PressKeyA();
-	inputHandler_->AssignMoveRightCommand2PressKeyD();
-
-	ICommand* command_ = nullptr;
-	Player* player_ = new Player();
-
+	
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
@@ -34,12 +93,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
-		command_ = inputHandler_->HandleInput();
-
-		if (command_) {
-			command_->Exec(*player_);
-		}
-		player_->Update();
+		
 		///
 		/// ↑更新処理ここまで
 		///
@@ -47,7 +101,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-		player_->Draw();
+		Novice::ScreenPrintf(0, 0, "rotateMatrix");
+		MatrixScreenPrintf(0, 20, rotateMatrix, "rotateMatrix");
 		///
 		/// ↑描画処理ここまで
 		///
